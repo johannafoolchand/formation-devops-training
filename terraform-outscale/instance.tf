fe321ccc9ccdc10df89e6bdf4ba84ae0268809e2
Aurelien Duboc <aurelienduboc96@gmail.com>
2024-09-01 19:03:47 +0200

    feat: update terraform

--- terraform-outscale/instance.tf
+++ terraform-outscale/instance.tf
@@ -1,6 +1,6 @@
 resource "outscale_vm" "vm_workshop" {
   image_id         = "ami-1111f1b5"
-  vm_type          = "tinav4.c1r1p3"
+  vm_type          = "tinav7.c2r2p3"
   keypair_name     = outscale_keypair.keypair_workshop.keypair_name
   security_group_ids = [
     outscale_security_group.security_group_workshop.security_group_id,
--- terraform-outscale/vars.tf
+++ terraform-outscale/vars.tf
@@ -10,8 +10,3 @@ variable "region" {
   type    = string
   default = "us-east-2"
 }
-
-variable "keypair_name" {
-  type    = string
-  default = "keypair-johanna"
-}
