78db475dfffb7f8e6e8afa7a3e71c773d5b01970
johannafoolchand <johanna.foolchand@outlook.fr>
2024-09-13 19:48:25 +0200

    check unit tests

--- tests/test_unitaires.py
+++ tests/test_unitaires.py
@@ -3,8 +3,8 @@ from flask_testing import TestCase
 from main import app, db, Task  # On importe directement depuis main.py
 
 class YourAppTestCase(TestCase):
-
-def client():
-    print("ok ok")      
+    def client():
+        print("ok ok")      
+        
 if __name__ == '__main__':
     unittest.main()
