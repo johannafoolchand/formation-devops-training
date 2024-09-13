82675fc3cb182b002ae237784649c63c5c68123d
johannafoolchand <johanna.foolchand@outlook.fr>
2024-09-13 19:50:46 +0200

    check unit tests

--- tests/test_unitaires.py
+++ tests/test_unitaires.py
@@ -3,8 +3,12 @@ from flask_testing import TestCase
 from main import app, db, Task  # On importe directement depuis main.py
 
 class YourAppTestCase(TestCase):
-    def client():
-        print("ok ok")      
-        
+    def create_app(self):
+        app.config['TESTING'] = True
+        # Utilisation d'une base de données de test en mémoire
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        return app
+
 if __name__ == '__main__':
     unittest.main()
